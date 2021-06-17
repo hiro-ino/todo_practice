1c9951e7469a60c64a35e35f27b084ed9f5bfccc
Inoue Hiroki <hiroki@InouenoMacBook-Pro.local>
2021-06-17 22:38:06 +0900

    Update store .php
    アップデート

--- store.php
+++ store.php
@@ -1,13 +1,16 @@
 <?php
 
 // ファイルの読み込み
-
-
+require_once('./Models/Task.php');
 // データの受け取り
-
-
+// スーパーグローバル変数
+$title = $_POST['title'];
+$contents = $_POST['contents'];
+$time = date("Y/m/d H:i:s");
 // DBへのデータ保存
-
-
+// インスタンス化
+$task = new Task();
+$task->create([$title, $contents, $currentTime]);
 // リダイレクト
-
+header('location:index.php');
+exit;
